52248ee91cbd555ff64a9c595276247de9bf2933
afzalqodirov <krisalya4@gmail.com>
2025-06-27 05:00:40 +0500

    routers added to main.py

--- main.py
+++ main.py
@@ -2,10 +2,15 @@ from fastapi import FastAPI
 
 #my imports
 from users import user_router, auth_router
+from versioning import v1_router, v2_router
+from books import book_router
 
 app = FastAPI()
 app.include_router(user_router)
 app.include_router(auth_router)
+app.include_router(v1_router)
+app.include_router(v2_router)
+app.include_router(book_router)
 
 @app.get('/')
 async def greeter():
